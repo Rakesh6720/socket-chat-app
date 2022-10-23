c54e2a7338bd9c93dd7e32c72a98e5393cb0fb43
Rakesh Baruah <rbaruah@gmail.com>
2022-10-23 13:20:09 -0500

    remove style classes from chat.html\

--- public/js/main.js
+++ public/js/main.js
@@ -44,10 +44,10 @@ chatForm.addEventListener("submit", (e) => {
 
 function outPutMessage(message) {
   const div = document.createElement("div");
-  div.classList.add("message");
+  //div.classList.add("message");
   div.innerHTML = `            
-    <p class="meta">${message.username} <span>${message.time}</span></p>
-    <p class="text">
+    <p>${message.username} <span>${message.time}</span></p>
+    <p>
         ${message.text}
     </p>
   `;
